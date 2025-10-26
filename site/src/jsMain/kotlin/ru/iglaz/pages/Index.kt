07f4db2b19e663bc6ba639405b8726959daabb07
IlyaGlaz <glazterix@gmail.com>
2025-10-26 18:30:49 +0300

    working with navBar

--- site/src/jsMain/kotlin/ru/iglaz/pages/Index.kt
+++ site/src/jsMain/kotlin/ru/iglaz/pages/Index.kt
@@ -5,13 +5,15 @@ import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.background
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.icons.fa.FaGithub
 import com.varabyte.kobweb.silk.components.navigation.Link
-import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.em
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.H1
@@ -23,16 +25,40 @@ fun HomePage() {
     Div(
         Modifier
             .fillMaxSize()
-            .background(color = com.varabyte.kobweb.compose.ui.graphics.Color.rgb(237, 237, 237))
             .toAttrs()
     ) {
-        Box(Modifier.height(50.px).background(color = Color.white), Alignment.Center) {
-            Row {
-                Link("/", "Домашняя страница")
-                Link("report/", "Отчеты")
+        Box(
+            Modifier
+                .height(56.px)
+                .boxShadow(spreadRadius = 1.px, color = Color.rgba(17, 17, 17, 0.2f)),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth(90.percent)
+                    .fontSize(1.5.cssRem),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Link(
+                    "https://github.com/IlyaGlaz/battle-report",
+                    Modifier.margin(right = 32.px)
+                ) {
+                    FaGithub()
+                }
+
+                Link(
+                    "/user", "Мои отчеты",
+                    Modifier.margin(left = 1.em, right = 1.em)
+                )
+                Link(
+                    "/battle", "Поле битвы",
+                    Modifier.margin(left = 1.em, right = 1.em)
+                )
             }
         }
+
         H1 { Text("Домашняя страница") }
     }
 }
+
 
